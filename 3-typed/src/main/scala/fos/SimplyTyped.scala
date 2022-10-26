91ade07af01a63450409c5affb17159fece4e9a0
amey-kulkarni27 <amey2704@gmail.com>
2022-10-26 11:19:37 +0200

    let's do typed

---
+++ 3-typed/src/main/scala/fos/SimplyTyped.scala
@@ -0,0 +1,93 @@
+package fos
+
+import scala.util.parsing.combinator.syntactical.StandardTokenParsers
+import scala.util.parsing.input._
+
+/** This object implements a parser and evaluator for the
+ *  simply typed lambda calculus found in Chapter 9 of
+ *  the TAPL book.
+ */
+object SimplyTyped extends StandardTokenParsers {
+  lexical.delimiters ++= List("(", ")", "\\", ".", ":", "=", "->", "{", "}", ",", "*")
+  lexical.reserved   ++= List("Bool", "Nat", "true", "false", "if", "then", "else", "succ",
+                              "pred", "iszero", "let", "in", "fst", "snd")
+
+  /** t ::=          "true"
+   *               | "false"
+   *               | number
+   *               | "succ" t
+   *               | "pred" t
+   *               | "iszero" t
+   *               | "if" t "then" t "else" t
+   *               | ident
+   *               | "\" ident ":" T "." t
+   *               | t t
+   *               | "(" t ")"
+   *               | "let" ident ":" T "=" t "in" t
+   *               | "{" t "," t "}"
+   *               | "fst" t
+   *               | "snd" t
+   */
+  def term: Parser[Term] =
+    ???
+
+
+  /** Thrown when no reduction rule applies to the given term. */
+  case class NoRuleApplies(t: Term) extends Exception(t.toString)
+
+  /** Print an error message, together with the position where it occured. */
+  case class TypeError(t: Term, msg: String) extends Exception(msg) {
+    override def toString =
+      msg + "\n" + t
+  }
+
+  /** The context is a list of variable names paired with their type. */
+  type Context = List[(String, Type)]
+
+  /** Call by value reducer. */
+  def reduce(t: Term): Term =
+    ???
+
+
+  /** Returns the type of the given term <code>t</code>.
+   *
+   *  @param ctx the initial context
+   *  @param t   the given term
+   *  @return    the computed type
+   */
+  def typeof(ctx: Context, t: Term): Type =
+    ???
+
+
+  /** Returns a stream of terms, each being one step of reduction.
+   *
+   *  @param t      the initial term
+   *  @param reduce the evaluation strategy used for reduction.
+   *  @return       the stream of terms representing the big reduction.
+   */
+  def path(t: Term, reduce: Term => Term): LazyList[Term] =
+    try {
+      var t1 = reduce(t)
+      LazyList.cons(t, path(t1, reduce))
+    } catch {
+      case NoRuleApplies(_) =>
+        LazyList.cons(t, LazyList.empty)
+    }
+
+  def main(args: Array[String]): Unit = {
+    val stdin = new java.io.BufferedReader(new java.io.InputStreamReader(System.in))
+    val tokens = new lexical.Scanner(stdin.readLine())
+    phrase(term)(tokens) match {
+      case Success(trees, _) =>
+        try {
+          println("typed: " + typeof(Nil, trees))
+          for (t <- path(trees, reduce))
+            println(t)
+        } catch {
+          case tperror: Exception => println(tperror.toString)
+        }
+      case e =>
+        println(e)
+    }
+  }
+}
