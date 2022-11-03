5bbf4816bfaf69144c8a5deb5aba1e6754f4cafb
Marija Lazaroska <marijalazaroska@Marijas-MacBook-Pro.local>
2022-11-03 01:06:21 +0100

    term and type parser done, half of reduce done

--- 3-typed/src/main/scala/fos/SimplyTyped.scala
+++ 3-typed/src/main/scala/fos/SimplyTyped.scala
@@ -2,6 +2,7 @@ package fos
 
 import scala.util.parsing.combinator.syntactical.StandardTokenParsers
 import scala.util.parsing.input._
+import scala.annotation.newMain
 
 /** This object implements a parser and evaluator for the
  *  simply typed lambda calculus found in Chapter 9 of
@@ -12,6 +13,8 @@ object SimplyTyped extends StandardTokenParsers {
   lexical.reserved   ++= List("Bool", "Nat", "true", "false", "if", "then", "else", "succ",
                               "pred", "iszero", "let", "in", "fst", "snd")
 
+
+  // -------------- TERMS ----------------------------//
   /** t ::=          "true"
    *               | "false"
    *               | number
@@ -36,12 +39,12 @@ object SimplyTyped extends StandardTokenParsers {
                           | "pred"~term^^{ case _~term => Pred(term)}
                           | "iszero"~term^^{ case _~term => IsZero(term) }
                           | ident^^{x=>Var(x)}
-                          | ("\\"~>ident)~(":"~>lambda_type)~("."~>term)^^{ case variable ~ l_type ~ term1=> Abs(variable, l_type, term1)} //TOCHANGE
+                          | ("\\"~>ident)~(":"~>lambda_type)~("."~>term)^^{ case variable ~ l_type ~ term1=> Abs(variable, l_type, term1)} 
                           | "("~term~")"^^{case _~x~_=>x}
-                         //| pair
-                         //| let
-                         //| fst
-                         //| snd
+                          | ("let"~>ident)~(":"~>lambda_type)~("="~>term)~("in"~>term)^^{case variable ~ l_type ~ t1 ~ t2 => App(Abs(variable,l_type,t2),t1)}
+                          | ("{"~>term)~","~(term<~"}")^^{case t1~_~t2 => TermPair(t1, t2)}
+                          | ("fst"~>term)^^{case t => First(t)}
+                          | ("snd"~>term)^^{case t => Second(t)}
 
 
   def numericLitRecursive(x: Int): Term = x match {
@@ -52,10 +55,8 @@ object SimplyTyped extends StandardTokenParsers {
   def term: Parser[Term] =
     termlet ~ rep(termlet) ^^ {case tlet ~ tlist => tlist.foldLeft(tlet)(App(_, _))}
 
-
-  // rep1sep[T](p: ⇒ Parser[T], q: ⇒ Parser[Any]) 
-  // repeatedly applies p interleaved with q to parse the input, until p fails. The parser p must succeed at least once
-  // so p=the term and q="->"
+  
+  // -------------- TYPES ----------------------------//
 
   def lambda_typelet: Parser[Type] = "Bool"^^^TypeBool
                                   | "Nat"^^^TypeNat
@@ -65,8 +66,16 @@ object SimplyTyped extends StandardTokenParsers {
   // def lambda_type: Parser[Type] =
   //   lambda_typelet ~ repsep(lambda_typelet,"->")^^{case type1 ~ types => types.foldRight(type1)(TypeFun(_, _))}
  
+  // rep1sep[T](p: ⇒ Parser[T], q: ⇒ Parser[Any]) 
+  // repeatedly applies p interleaved with q to parse the input, until p fails. The parser p must succeed at least once
+  // so p=the term and q="->"
+
+  def pair_type: Parser[Type] = 
+    rep1sep(lambda_typelet,"*")^^{case types => types.reduceRight(TypePair(_, _))}
+  //first applying the * because it has precedence 
   def lambda_type: Parser[Type] =
-    rep1sep(lambda_typelet,"->")^^{case types => types.reduceRight(TypeFun(_, _))}
+    rep1sep(pair_type,"->")^^{case types => types.reduceRight(TypeFun(_, _))}
+
 
   /** Thrown when no reduction rule applies to the given term. */
   case class NoRuleApplies(t: Term) extends Exception(t.toString)
@@ -80,9 +89,120 @@ object SimplyTyped extends StandardTokenParsers {
   /** The context is a list of variable names paired with their type. */
   type Context = List[(String, Type)]
 
+  //----------- [START] functions from previous HW -----------------------//
+  var counter: Int = 0
+  def alpha(t: Abs): Abs = {
+    var new_name: String = t.v+counter.toString
+    counter+=1
+    
+    def replace(term:Term, valueToBeReplaced: String, new_value: String): Term = {
+      term match {
+        case Var(x) => if(x==valueToBeReplaced){Var(new_name)}else{Var(x)}
+        case Abs(x,type1,t1) => if(x==valueToBeReplaced){Abs(x,type1,t1)}else{Abs(x, type1, replace(t1, valueToBeReplaced, new_value))}
+        case App(t1, t2) => App(replace(t1, valueToBeReplaced, new_value),replace(t2, valueToBeReplaced, new_value))
+
+        //arithmetic
+        case True => True
+        case False => False
+        case Succ(t) => Succ(replace(t, valueToBeReplaced, new_value))
+        case Pred(t) => Pred(replace(t, valueToBeReplaced, new_value))
+        case If(cond, t1, t2) => If(replace(cond, valueToBeReplaced, new_value), replace(t1, valueToBeReplaced, new_value), replace(t2, valueToBeReplaced, new_value))
+        case IsZero(t) => IsZero(replace(t, valueToBeReplaced, new_value))
+
+        //new terms from typed
+      }
+    } 
+    var new_term: Term = replace(t.t, t.v, new_name)
+    return Abs(new_name,t.tp,new_term)
+
+  }
+
+  def subst(t: Term, x: String, s: Term): Term = {
+    
+    def isFV(s: Term, y: String): Boolean = {
+      s match{
+        case Var(z) => if(z == y){return true}else{return false}
+        case Abs(z,type1,t1) => if (z==y){return false}else{return isFV(t1, y)}
+        case App(t1, t2) => return isFV(t1, y) || isFV(t2, y)
+        // arithmetics
+        case True => false //not sure about these two
+        case False => false
+        case Succ(t) => isFV(t,y)
+        case Pred(t) => isFV(t,y)
+        case If(cond, t1, t2) => isFV(cond,y) || isFV(t1,y) || isFV(t2,y)
+        case IsZero(t) => isFV(t,y)
+      }
+    }
+
+    t match {
+      case Var(y) => if(y==x){s}else{Var(y)}
+      case Abs(y,type1,t1) => if(y==x){Abs(y,type1,t1)}else{if(!isFV(s, y)){Abs(y,type1,subst(t1, x, s))}else{
+         subst(alpha(Abs(y,type1,t1)), x, s)
+      }}
+      case App(t1, t2) => App(subst(t1, x, s), subst(t2, x, s))
+      // arithmetics
+      case True => True
+      case False => False
+      case Succ(t) => Succ(subst(t,x,s))
+      case Pred(t) => Pred(subst(t,x,s))
+      case If(cond, t1, t2) => If(subst(cond, x, s), subst(t1, x, s), subst(t2, x, s))
+      case IsZero(t) => IsZero(subst(t,x,s))
+    }
+  }
+
+  // Can the term potentially evaluate by call by value
+  def shouldSubst(term: Term): Boolean = term match {
+    case Var(_) => false 
+    case Abs(_,_,_) => false 
+    case App(t1,t2) => t1 match {
+      case Abs(_,_,_) => t2 match
+        case Abs(_,_,_) => true // the value that is being substituted should also be a "value", not a variable
+        case _ => shouldSubst(t2)
+      
+      case _ => shouldSubst(t1)
+    }
+    //arithmetics  Don't know if we should add the values from arithmetics here
+    // case True => false
+    // case False => false
+    // case Zero => false
+    // case Succ(t) => shouldSubst(t)
+  }
+
+//----------- [END] functions from previous HW -----------------------//
+
   /** Call by value reducer. */
-  def reduce(t: Term): Term =
-    ???
+  def reduce(t: Term): Term = t match {
+    //arithmetic
+    case If(True, t1, _) => t1
+    case If(False, _, t2) => t2
+    case IsZero(Zero) => True
+    case IsZero(Succ(nv)) => False
+    case Pred(Zero) => Zero
+    case Pred(Succ(nv)) => nv
+    case If(t1, t2, t3) => If(reduce(t1), t2, t3) 
+    case IsZero(t1) => IsZero(reduce(t1))
+    case Pred(t1) => Pred(reduce(t1))
+    case Succ(t1) => Succ(reduce(t1))
+
+
+    //untyped
+    
+    case App(tt, s) => tt match {
+      case Abs(x1,type1,t1) => {
+        if(shouldSubst(s))
+          App(tt, reduce(s))
+        else 
+          s match {
+            case Var(_) => throw new NoRuleApplies(t)
+            case _ => subst(t1, x1, s)
+          }
+      }
+      case Var(_) => throw new NoRuleApplies(t)
+      case App(_, _) => App(reduce(tt), s)
+    }
+    case _ => throw new NoRuleApplies(t)
+  }
+    
 
 
   /** Returns the type of the given term <code>t</code>.
